02d9108d9845e3e5422e11e04c87048828db1071
Guillermo <guillermo.barrasa@enti.cat>
2021-02-02 11:23:33 +0100

    Protocolo parcialmente hecho

---
+++ abfp/client.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PORT=2021
+IP_CLIENT="127.0.0.1"
+IP_SERVER="127.0.0.1"
+
+echo "Client ABFP"
+
+echo "(2) Sending Headers"
+sleep 1
+echo "ABFP $IP_CLIENT" | nc -q 1 $IP_SERVER $PORT
+
+echo "(3) Listening: $PORT"
+RESPONSE=`nc -l -p $PORT`
+
+echo "TEST! $RESPONSE"
+if [ "$RESPONSE" != "OK_CONN" ]; then
+
+	echo "Connexion with the server error"
+	exit 1
+
+fi
+
+echo "(6) Sending Handshake"
+sleep 1
+echo "THIS_IS_MY_CLASSROOM" | nc -q 1 $IP_SERVER $PORT
+
+echo "(7) Listening:"
+RESPONSE=`nc -l -p $PORT`
+
+exit 0
---
+++ abfp/server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PROTOCOL="ABFP"
+PORT=2021
+
+echo "(0) Server ABFP"
+
+echo "(1) Listening: $PORT"
+HEADER=`nc -l -p $PORT`
+
+echo "TEST! $HEADER"
+PREFIX=`echo $HEADER | cut -d " " -f 1`
+IP_CLIENT=`echo $HEADER | cut -d " " -f 2`
+
+echo "(4) Response" 
+if [ "$PREFIX" != $PROTOCOL ]; then
+
+	echo "Header error"
+	sleep 1
+	echo "KO_CONN" | nc -q 1 $IP_CLIENT $PORT
+	exit 1
+
+fi
+
+sleep 1
+echo "OK_CONN" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(5) Listening:"
+HANDSHAKE=`nc -l -p $PORT`
+
+echo "TEST! $HANDSHAKE"
+if [ "$HANDSHAKE" != "THIS_IS_MY_CLASSROOM" ]; then
+
+	echo "Handshake error"
+	sleep 1
+	echo "KO_HANDSHAKE" | nc -q 1 $IP_CLIENT $PORT
+
+fi
+
+exit 0
